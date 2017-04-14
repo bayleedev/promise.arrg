eb47cb9dbb90c7ac9d29cccdd044f12c01aaa9e4
Blaine Schmeisser <blainesch@gmail.com>
2017-04-14 08:37:25 -0700

    Move to arrg.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Pirates want treasure!
-const { crew, capin } = require('./.arg')
+const { crew, capin } = require('./.arrg')
 
 // 1) Generate a greeting and print it to the console
 // TIP: capin.generateGreeting() // returns a promise!
